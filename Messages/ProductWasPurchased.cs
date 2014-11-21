1f4608af52ed8eed039eff15ff9dd122b227ce63
christiaanverwijs <christiaan.verwijs@hroffice.nl>
2014-11-21 16:25:49 +0100

    Whoops. Typo in ProductWasPurchased Event, which is not a command :)

--- Messages/ProductWasPurchased.cs
+++ Messages/ProductWasPurchased.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Samples.NServiceBus.Messages.Commands
+namespace Samples.NServiceBus.Messages.Events
 {
     public class ProductWasPurchased : ProductEventBase
     {
